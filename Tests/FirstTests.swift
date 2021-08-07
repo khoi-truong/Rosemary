3a8cd3aaf1ad7be23a091325e0053d7a6cc7e327
Khoi Truong <khoi.truongminh@gmail.com>
2021-08-07 22:41:16 +0700

    feat: add Bazel target for Unit Tests

---
+++ Tests/FirstTests.swift
@@ -0,0 +1,16 @@
+//
+//  RosemaryTests.swift
+//  RosemaryTests
+//
+//  Created by Khoi Truong Minh on 7/27/21.
+//
+
+import XCTest
+@testable import Rosemary
+
+class FirstTests: XCTestCase {
+
+    func testExample() {
+        XCTAssertEqual(49, 49)
+    }
+}
---
+++ Tests/SecondTests.swift
@@ -0,0 +1,16 @@
+//
+//  RosemaryTests.swift
+//  RosemaryTests
+//
+//  Created by Khoi Truong Minh on 7/27/21.
+//
+
+import XCTest
+@testable import Rosemary
+
+class SecondTests: XCTestCase {
+
+    func testAbc() {
+        XCTAssert(true)
+    }
+}
